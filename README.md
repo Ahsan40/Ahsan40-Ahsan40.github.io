b9996a234d859470b7d3c58275c4ca4cae7113a7
Ahsan40 <30434801+Ahsan40@users.noreply.github.com>
2020-03-10 02:21:32 +0600

    New Template

--- README.md
+++ README.md
@@ -1,37 +1,43 @@
-################################################################################################
 
-Ejalee Inc. 4th September, 2016 22:35GMT+1
+TITLE: 
+Personal - 100% Fully Responsive Free HTML5 Bootstrap Template
 
-################################################################################################
+AUTHOR:
+DESIGNED & DEVELOPED by FREEHTML5.co
 
-Hi,
+Website: http://freehtml5.co/
+Twitter: http://twitter.com/fh5co
+Facebook: http://facebook.com/fh5co
 
-Thank you for viewing my biography. Have fun and tell your friends about me ;)
 
+CREDITS:
 
-## CSS
+Bootstrap
+http://getbootstrap.com/
 
-The theme stylesheet is css/style.default.css. If you want to make any changes, you can do it here or better to override it in custom.css so you can update the original theme stylesheet if an updated is released.
+jQuery
+http://jquery.com/
 
-## Javascript
+jQuery Easing
+http://gsgd.co.uk/sandbox/jquery/easing/
 
-Apart from Bootstrap JS components and jquery.onepage-scroll majority of JS is located in /js/front.js. Onepage scrolling is activated for screen widths > 1000px, if you want to change it, do so in front.js.
+Modernizr
+http://modernizr.com/
 
-## Credits
-- Botstrap 3.3.2
-- Font Awesome 4.3.0
-- jQuery One Page scroll - https://github.com/peachananr/onepage-scroll
-- Google Fonts - Montserrat + Cardo
+Google Fonts
+https://www.google.com/fonts/
 
- ## LICENCE CONDITIONS
+Icomoon
+https://icomoon.io/app/
 
-You are completely free to use this template for your personal use or as a work for your client as long as you keep the link at the template footer pointing to us and our partner.
+Respond JS
+https://github.com/scottjehl/Respond/blob/master/LICENSE-MIT
 
-However you cannot redistribute the template as a template itself - neither for free or commercially (i.e. selling it on template marketplace).
+animate.css
+http://daneden.me/animate
 
-Thank you for understanding and respecting the licence conditions.
+jQuery Waypoint
+https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
 
- ## GET IN TOUCH 
-
-https://twitter.com/azemighty76 | https://plus.google.com/+adigunazeez | https://www.facebook.com/azemighty | 
-Skype: Azemighty
+Easy Pie Chart
+https://github.com/rendro/easy-pie-chart
--- js/main.js
+++ js/main.js
@@ -1,3 +1,95 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+;(function () {
+	
+	'use strict';
 
-},{}]},{},[1])
+
+
+	var isMobile = {
+		Android: function() {
+			return navigator.userAgent.match(/Android/i);
+		},
+			BlackBerry: function() {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+			iOS: function() {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+			Opera: function() {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+			Windows: function() {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+			any: function() {
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+		}
+	};
+
+	var getHeight = function() {
+		var extraHeight = 0;
+
+		if ( isMobile.any() ) extraHeight = 50;
+		
+		setTimeout(function(){
+			$('#fh5co-main').stop().animate({
+				'height': $('.fh5co-tab-content.active').height() + extraHeight
+			});
+		}, 200);
+	};
+
+	var pieChart = function() {
+		$('.chart').easyPieChart({
+			scaleColor: false,
+			lineWidth: 10,
+			lineCap: 'butt',
+			barColor: '#17e7a4',
+			trackColor:	"#000000",
+			size: 160,
+			animate: 1000
+		});
+	};
+
+	var tabContainer = function() {
+		getHeight();
+		$(window).resize(function(){
+			getHeight();
+		})
+	};
+
+	var tabClickTrigger = function() {
+		$('.fh5co-tab-menu a').on('click', function(event) {
+			event.preventDefault();
+			var $this = $(this),
+				data = $this.data('tab'),
+				pie = $this.data('pie');
+
+			// add/remove active class
+			$('.fh5co-tab-menu li').removeClass('active');
+			$this.closest('li').addClass('active');
+
+			$('.fh5co-tab-content.active').addClass('animated fadeOutDown');
+
+			setTimeout(function(){
+				$('.fh5co-tab-content.active').removeClass('active animated fadeOutDown fadeInUp');
+				$('.fh5co-tab-content[data-content="'+data+'"]').addClass('animated fadeInUp active');
+				getHeight();
+			}, 500);
+
+			if ( pie === 'yes' ) {
+				setTimeout(function(){
+					pieChart();
+				}, 800);
+			}
+			
+		})
+	};
+
+	// Document on load.
+	$(function(){
+		tabContainer();
+		tabClickTrigger();
+
+	});
+
+
+}());
